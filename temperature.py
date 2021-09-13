cd949dc55157789566494af82e725cad2a78689d
Chia-pei Ho <pei19860202@gmail.com>
2021-09-13 20:26:42 +0200

    add new file

---
+++ temperature.py
@@ -0,0 +1,4 @@
+temperature_celsius = input('攝氏溫度:')
+temperature_celsius = float(temperature_celsius)
+temperature_fahrenheit = temperature_celsius * 9 / 5 + 32
+print('華氏溫度: ', temperature_fahrenheit)
