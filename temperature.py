b7191069960622ee5ac519633bd8442847085c16
Chia-pei Ho <pei19860202@gmail.com>
2021-09-13 20:46:52 +0200

    add fever detection

--- temperature.py
+++ temperature.py
@@ -1,4 +1,9 @@
 temperature_celsius = input('攝氏溫度:')
+
 temperature_celsius = float(temperature_celsius)
 temperature_fahrenheit = temperature_celsius * 9 / 5 + 32
+
 print('華氏溫度: ', temperature_fahrenheit)
+
+if temperature_fahrenheit >= 100.4:
+    print('warning:fever')
